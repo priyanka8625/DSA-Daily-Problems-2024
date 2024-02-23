067c50540b28a37e229f16738b2db82d747b0865
AnkitaAdam <ksundalam@gmail.com>
2024-02-23 13:23:30 +0530

    GFG POTD Soln added

---
+++ 02_February_Daily_Problems/23_GFG_POTD_Buy and Sell a Share at most twice.cpp
@@ -0,0 +1,35 @@
+class Solution
+
+{
+
+    public:
+
+        //User function Template for C++   
+
+        int maxProfit(vector<int>&prices){
+
+            //Write your code here..
+
+            int buy1 = INT_MIN, sell1 = 0;
+
+            int buy2 = INT_MIN, sell2 = 0;
+
+ 
+
+            for (int price : prices) {
+
+                buy1 = max(buy1, -price);
+
+                sell1 = max(sell1, buy1 + price);
+
+                buy2 = max(buy2, sell1 - price);
+
+                sell2 = max(sell2, buy2 + price);
+
+            }
+
+            return sell2;
+
+        }
+
+};
