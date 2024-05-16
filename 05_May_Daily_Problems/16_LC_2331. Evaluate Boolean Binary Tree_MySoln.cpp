30b74ec40fce076ce2d5830b9b22869d11923e13
AnkitaAdam <ksundalam@gmail.com>
2024-05-16 23:24:09 +0530

    Leetcode POTD Soln

---
+++ 05_May_Daily_Problems/16_LC_2331. Evaluate Boolean Binary Tree_MySoln.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        //for leaf nodes
+        if(root->left == NULL && root->right == NULL){
+            return root->val;//return true or false 
+        }
+
+        //for non leaf nodes
+        //evaluate left subtree 
+        bool leftRes = evaluateTree(root->left);
+        //evaluate right subtree
+        bool rightRes = evaluateTree(root->right);
+        
+        return (root->val==2)? (leftRes || rightRes) : (leftRes && rightRes);
+    }
+};
