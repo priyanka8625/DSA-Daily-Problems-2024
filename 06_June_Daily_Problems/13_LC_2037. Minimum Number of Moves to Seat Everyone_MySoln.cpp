2e0d77f52e6fb3e22463b5b5f17d237c2a95be70
AnkitaAdam <ksundalam@gmail.com>
2024-06-13 22:03:44 +0530

    Leetcode POTD Soln

---
+++ 06_June_Daily_Problems/13_LC_2037. Minimum Number of Moves to Seat Everyone_MySoln.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int n = seats.size(), minMoves=0;
+        for(int i=0; i<n; i++){
+            minMoves += abs(seats[i]-students[i]);
+        }
+        return minMoves;
+    }
+};
