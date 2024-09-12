121ef0a0cf9d2e65362da518429190fd37e42724
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-09-12 18:57:53 +0530

    GFG POTD Soln

---
+++ 09_September_Daily_Problems/12_GFG_Middle of a Linked List_MySoln.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    /* Should return data of middle node. If linked list is empty, then -1 */
+    int getMiddle(Node* head) {
+        // code here
+        Node* slow=head, *fast=head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow->data;
+    }
+};
