4b1985d98d0214cd67306ad62c59058e7ae60314
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-10-07 19:55:08 +0530

    Leetcode potd soln

---
+++ 10_October_Daily_Problems/07_LC_2696. Minimum String Length After Removing Substrings_MySoln.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(char ch : s){
+            if(ch=='B' && !st.empty() && st.top()=='A'){
+                st.pop();
+            }else if(ch=='D' && !st.empty() && st.top()=='C'){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        return st.size();
+    }
+};
