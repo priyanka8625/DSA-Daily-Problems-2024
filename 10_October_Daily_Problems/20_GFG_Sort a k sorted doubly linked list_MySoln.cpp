7c78ca99c7aadf038ff5498fa67d082a6262b650
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-10-20 11:34:17 +0530

    GFG POTD Soln

---
+++ 10_October_Daily_Problems/20_GFG_Sort a k sorted doubly linked list_MySoln.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    // function to sort a k sorted doubly linked list
+    DLLNode *sortAKSortedDLL(DLLNode *head, int k) {
+        // code here
+        priority_queue<int, vector<int>, greater<int>> pq;
+        DLLNode *temp = head;
+        DLLNode *curr = head;
+        while(temp){
+            pq.push(temp->data);
+            if(pq.size()==k+1){
+                curr->data = pq.top();
+                pq.pop();
+                curr = curr->next;
+            }
+            temp = temp->next;
+        }
+        while(!pq.empty()){
+            curr->data = pq.top();
+            pq.pop();
+            curr = curr->next;
+        }
+        return head;
+    }
+};
